ebb72c5ab7ea628d2a6b4350b2b9da4e6d301dcb
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-12-19 12:11:31 -0700

    fix styles on Home Screen

--- src/Components/Screens/Home.js
+++ src/Components/Screens/Home.js
@@ -11,63 +11,75 @@ class HomeScreen extends Component {
     const {navigate} = this.props.navigation;
     return (
       <>
-        <SafeAreaView>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Welcome!</Text>
-            </View>
+        <SafeAreaView style={styles.Container}>
+          <View style={styles.headerSection}>
+            <Text style={styles.headerTitle}>Welcome to Books List!</Text>
           </View>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>
-                This is a place where you can keep track of books you want to
-                read. Enter the title and author on the next screen. Once you
-                get around to reading a book, make sure you mark it off as
-                'read'.
-              </Text>
-            </View>
+          <View style={styles.section}>
+            <Text style={styles.sectionParagraph}>
+              Books List is a space where you keep track of books you want to
+              read.
+            </Text>
           </View>
-          <View style={styles.body}>
-            <View style={styles.sectionContaine}>
-              <Button
-                style={{fontSize: 20, color: 'white'}}
-                containerStyle={{
-                  padding: 20,
-                  margin: 30,
-                  overflow: 'hidden',
-                  borderRadius: 4,
-                  backgroundColor: 'blue',
-                }}
-                onPress={() => navigate('AddBooks')}>
-                Next
-              </Button>
-            </View>
+          <View style={styles.section}>
+            <Text style={styles.sectionParagraph}>
+              Enter the title and author on the next screen.
+            </Text>
           </View>
+
+          <View style={styles.section}>
+            <Text style={styles.sectionParagraph}>
+              Future features includes: ability to mark book as 'read'.
+            </Text>
+          </View>
+          <Button
+            style={{fontSize: 20, color: 'white'}}
+            containerStyle={{
+              padding: 20,
+              margin: 30,
+              overflow: 'hidden',
+              borderRadius: 4,
+              backgroundColor: 'blue',
+            }}
+            onPress={() => navigate('AddBooks')}>
+            Next
+          </Button>
         </SafeAreaView>
       </>
     );
   }
 }
 const styles = StyleSheet.create({
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
+  cContainer: {
     backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
     color: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 50,
   },
-
-  highlight: {
-    fontWeight: '700',
+  headerSection: {
+    marginTop: 32,
+    marginBottom: 32,
+    paddingHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+  section: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  sectionParagraph: {
+    fontSize: 18,
+    fontWeight: '400',
   },
 });
 export default HomeScreen;
