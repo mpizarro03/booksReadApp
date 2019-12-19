eeaa12ff8c8c173a472e47afca63e22bde4cd7d8
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-12-18 20:18:18 -0700

    import react-native-gesture-halder, dependency of react-navigation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import App from './src/App';
 import {name as appName} from './app.json';
 import Amplify from '@aws-amplify/core';
 import config from './aws-exports';
+import 'react-native-gesture-handler';
 
 Amplify.configure(config);
 AppRegistry.registerComponent(appName, () => App);
